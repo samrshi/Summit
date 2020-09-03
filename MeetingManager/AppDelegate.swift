94bb0f1d87846e6c24458a20c89e67057ebc71b8
samrshi <samrshi@outlook.com>
2020-09-02 20:32:17 -0400

    changed default app height

--- MeetingManager/AppDelegate.swift
+++ MeetingManager/AppDelegate.swift
@@ -21,7 +21,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         // Create the popover
         let popover = NSPopover()
-        popover.contentSize = NSSize(width: 400, height: 463)
+        popover.contentSize = NSSize(width: 400, height: 470)
         popover.behavior = .transient
         // makes app always in dark mode
         popover.appearance = .some(NSAppearance(named: .vibrantDark)!)
