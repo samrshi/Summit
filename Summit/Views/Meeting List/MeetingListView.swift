51d3e914d73033cbafce092ccbc080fa4b236bb7
samrshi <samrshi@outlook.com>
2020-09-24 22:37:02 -0400

    Added new icons

--- Summit/Views/Meeting List/MeetingListView.swift
+++ Summit/Views/Meeting List/MeetingListView.swift
@@ -15,48 +15,18 @@ struct MeetingListView: View {
     @Binding var selectedMeetingID: UUID?
     @Binding var onlyShowToday: Bool
     
-    @State private var showFilter: Bool = false
-    @State private var filterString: String = ""
-    
     @State private var nextMeeting: Meeting = blankMeeting
     
     var body: some View {
         VStack {
-            VStack(alignment: .leading) {
-                if !userInfo.allMeetings.isEmpty && onlyShowToday {
-                    AnyView(NextMeetingView(mainViewState: $mainViewState, onlyShowToday: $onlyShowToday, selectedMeetingID: $selectedMeetingID, deleteMeetings: self.deleteMeetings))
-                } else if !userInfo.allMeetings.isEmpty && !onlyShowToday {
-                    AnyView(
-                        VStack(alignment: .leading) {
-                            Text("Upcoming Meetings from Calendar")
-                                .heading2()
-                            
-                            ForEach(userInfo.calendarEvents, id: \.id) { event in
-                                MeetingItemView(meeting: event, mainViewState: $mainViewState, onlyShowToday: $onlyShowToday, selectedMeetingID: $selectedMeetingID, hideOptions: true, show24HourTime: userInfo.settings.show24HourTime, delete: {})
-                            }
-                        }
-                    )
-                }
+            ZStack(alignment: .top) {
+                TodayView(mainViewState: $mainViewState, onlyShowToday: $onlyShowToday, selectedMeetingID: $selectedMeetingID, deleteMeetings: deleteMeetings)
                 
-                MeetingListHeader(onlyShowToday: $onlyShowToday, showFilter: $showFilter, filterString: $filterString)
-                
-                Divider()
-                                
-                ForEach(onlyShowToday ? userInfo.todaysMeetings : userInfo.allMeetings.filter(filterLogic), id: \.id) { meeting in
-                    MeetingItemView(meeting: meeting, mainViewState: self.$mainViewState, onlyShowToday: self.$onlyShowToday, selectedMeetingID: self.$selectedMeetingID, hideOptions: onlyShowToday, show24HourTime: self.userInfo.settings.show24HourTime) {
-                        self.deleteMeetings(meeting: meeting)
-                    }
-                }
-            }
-            
-            if userInfo.allMeetings.isEmpty {
-                EmptyStateView(mainViewState: $mainViewState, emptyStateType: .emptyList)
-            } else if onlyShowToday && userInfo.todaysMeetings.isEmpty {
-                EmptyStateView(mainViewState: $mainViewState, emptyStateType: .freeDay)
+                AllMeetingsView(mainViewState: $mainViewState, onlyShowToday: $onlyShowToday, selectedMeetingID: $selectedMeetingID, deleteMeetings: deleteMeetings)
             }
         }
         .transition(.opacity)
-        .padding()
+        .padding([.horizontal, .bottom])
         .onAppear {
             self.userInfo.updateDate()
             self.userInfo.getNextMeeting()
@@ -70,13 +40,5 @@ struct MeetingListView: View {
                 $0.id == id
             }
         }
-    }
-    
-    func filterLogic(meeting: RecurringMeetingModel) -> Bool {
-        if filterString.isEmpty {
-            return true
-        }
-        
-        return meeting.name.lowercased().contains(filterString.lowercased())
     }
 }
